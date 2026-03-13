7ecfbd207e389d4beeb9bc12848e2b61241912a6
Ethan-J-Pond <vhzpt0108@gmail.com>
2026-03-13 15:23:47 +0900

    Update 4-1.c

--- code/4-1.c
+++ code/4-1.c
@@ -2,7 +2,24 @@
 
 int main()
 {
-
+    char operator;
+    int x, y;  
+    scanf("%d %d %c",&x,&y,&operator);
+    switch(operator){
+        case '+' :
+            printf("%d\n",x+y);
+            break;
+        case '-' :
+            printf("%d\n",x-y);
+            break;
+        case '*' :
+            printf("%d\n",x*y);
+            break;
+        case '/' :
+            printf("%d\n",x/y);
+            break;
+        default:
+            printf("안돼!!!\n %d %d %d %d",'+','-','*','/');
+    }
     return 0;
 }
-

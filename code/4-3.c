8ec94512489bd3be08e613754e36e10cb4043a20
Ethan-J-Pond <vhzpt0108@gmail.com>
2026-03-13 15:24:20 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -2,7 +2,19 @@
 
 int main()
 {
+    int x, count = 1;
+    scanf("%d",&x);
+    
+    int i = x / 2 + 1;
 
+    for (int i = 1; i < x / 2 + 1; i++)
+        if ( x % i == 0 )
+            count++;
+        
+    
+    if(count == 2 )
+        printf("true");
+    else
+        printf("false");
     return 0;
 }
-
